5a682bc0890128aa86b74708e1585ba3ec0ebceb
Rajesh <hyperoot.tech@proton.me>
2026-05-17 06:34:40 +0000

    feat: add LaTeX support with MathJax integration and update styles for SVG rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
+import rehypeMathjax from 'rehype-mathjax'
+import remarkMath from 'remark-math'
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  },
+
   integrations: [
     react(),
     icon({
